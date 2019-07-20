3bc41e2dfffdaf35f052039721da2f4263ce1def
samanthajowilson <36824168+samanthajowilson@users.noreply.github.com>
2019-07-20 09:43:52 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,12 @@
+onEvent("startButton", "click", function(){
+setScreen("screen2");
+});
+onEvent("nextButton1", "click", function(){
+setScreen("screen3");
+});
+onEvent("nextButton2", "click", function(){
+setScreen("screen4");
+});
+onEvent("homeButton", "click", function(){
+setScreen("introScreen");
+});
